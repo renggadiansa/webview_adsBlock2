2ce0b42157c473d93ff013bd65c86746ddacb818
renggadiansa <renggadiansa@gmail.com>
2022-12-09 12:31:06 +0700

    first commit

---
+++ app/src/main/java/com/example/adsblock2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.adsblock2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.monstertechno.adblocker.AdBlockerWebView;
+import com.monstertechno.adblocker.util.AdBlocker;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webview);
+        new AdBlockerWebView.init(this).initializeWebView(webview);
+        webview.setWebViewClient(new Browser_home());
+
+        webview.loadUrl("https://lk21official.info/");
+    }
+    private class Browser_home extends WebViewClient {
+
+        Browser_home() {}
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+
+            return AdBlockerWebView.blockAds(view,url) ? AdBlocker.createEmptyResource() :
+                    super.shouldInterceptRequest(view, url);
+
+        }
+
+    }
+}
